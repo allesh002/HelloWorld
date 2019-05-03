1ba84160d65ff26248b7f11f3b3da553232cd849
allesh <allesh@protonmail.com>
2019-05-04 00:06:32 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("new string");
+        System.out.println("3rd string");
+
     }
 
 }
